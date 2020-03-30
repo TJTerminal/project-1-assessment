65298512b1a1ba6fcf0bbf9cfcdce4631f4ad663
Ted Jang <tedjang@Teds-MacBook-Pro.local>
2020-03-29 22:05:57 -0700

    button works but still not the sum

--- js/main.js
+++ js/main.js
@@ -2,8 +2,9 @@
 let count = 0;
 let playerGuess = 0;
 
-var inputValue = document.getElementById('input').parseInt();
+var inputValue = document.getElementById('input').value;
 var finalCount = document.getElementById('sum');
+
 // let playerGuess = parseInt(input.value)
 
 
@@ -11,23 +12,28 @@ let plusButton = document.getElementById('plus');
 let minusButton = document.getElementById('minus');
 // clickhandler
 plusButton.addEventListener('click', addition);
-minusButton.addEventListener('click', subtract)
+minusButton.addEventListener('click', subtract);
 
 
 function addition() {
-    // value entered in the <input> is added to the count and the new count value displayed
+    // value entered in the <input> is added to the count and the new count value is displayed
     count = parseInt(count) + parseInt(inputValue);
     updateCount();
+    console.log("addtion!");
+    console.log(count);
 }
 
 function subtract() {
-    // value entered in the <input> is subtracted from the count and the new count value displayed
+    // value entered in the <input> is subtracted from the count and the new count value is displayed
     count = parseInt(count) - parseInt(inputValue);
     updateCount();
+    console.log("subtract!");
+    console.log(count);
 }
 
 function updateCount() {
     finalCount.innerHTML = count;
+    console.log(count);
 }
 
 updateCount();
